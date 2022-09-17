5c76d3956827b8e60b6223d1b8c77bad1035e7ad
Hung-I Wang <whygowe@gmail.com>
2022-09-17 21:16:07 +0800

    add missing file fp.rs and totp.rs

---
+++ snowy-tunnel/src/fp.rs
@@ -0,0 +1,389 @@
+use hex_literal::hex;
+use ja3_rustls::{is_grease_u16_be, try_regrease_u16_be, Ja3};
+use rustls::internal::msgs::base::Payload;
+use rustls::internal::msgs::codec::Codec;
+use rustls::internal::msgs::enums::ExtensionType;
+use rustls::internal::msgs::handshake::{
+    ClientExtension, ConvertProtocolNameList, HandshakeMessagePayload, HandshakePayload,
+    KeyShareEntry, ProtocolNameList, UnknownExtension,
+};
+use rustls::internal::msgs::message::{Message, MessagePayload};
+
+use tracing::{debug, trace, warn};
+
+use std::collections::HashMap;
+
+use std::sync::Arc;
+
+use crate::try_assign;
+
+const RFC7685_PADDING_TARGET: usize = 512;
+
+/// TLS ClientHello fingerprints
+#[derive(Debug, Clone, Default)]
+pub struct FingerprintSpec {
+    pub ja3: Option<Ja3>,
+    pub alpn: Option<Vec<Vec<u8>>>,
+    pub signature_algos: Option<Vec<u16>>,
+    pub supported_versions: Option<Vec<u16>>,
+    pub key_share: Option<Vec<u16>>,
+}
+
+impl FingerprintSpec {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    // pub fn with_ja3(self, ja3: Ja3) -> Self {
+    //     self.ja3 = Some(ja3);
+    //     self
+    // }
+
+    pub fn is_empty(&self) -> bool {
+        self.ja3.is_none()
+            && self.alpn.is_none()
+            && self.signature_algos.is_none()
+            && self.supported_versions.is_none()
+            && self.key_share.is_none()
+    }
+
+    pub fn overwrite_client_hello(
+        &self,
+        msg: &mut Message,
+        add_empty_if_extension_not_in_message: bool,
+        drop_extensions_not_in_ja3: bool,
+    ) -> Option<Vec<ExtensionType>> {
+        overwrite_client_hello_with_fingerprint_spec(
+            msg,
+            self,
+            add_empty_if_extension_not_in_message,
+            drop_extensions_not_in_ja3,
+        )
+    }
+
+    pub fn get_client_hello_overwriter(
+        self: &Arc<Self>,
+        add_empty_if_extension_not_in_message: bool,
+        drop_extensions_not_in_ja3: bool,
+    ) -> Option<impl FnOnce(&mut Message) -> Option<Vec<ExtensionType>>> {
+        if self.is_empty() {
+            return None;
+        }
+        let fp = self.clone();
+        Some(move |msg: &mut Message| {
+            fp.overwrite_client_hello(
+                msg,
+                add_empty_if_extension_not_in_message,
+                drop_extensions_not_in_ja3,
+            )
+        })
+    }
+
+    // pub fn try_set_version(&self, ver: &mut ProtocolVersion) {
+    //     *ver = self.ja3.map(|ja3| ja3.version_to_typed()).unwrap_or(*ver)
+    // }
+
+    // pub fn try_set_ciphers(&self, ver: &mut ProtocolVersion) {
+    //     *ver = self.ja3.map(|ja3| ja3.version_to_typed()).unwrap_or(*ver)
+    // }
+}
+
+// impl From<Ja3> for FingerprintSpec {
+//     fn from(ja3:Ja3) -> Self {
+//         Self {
+//             ja3,
+//             alpn: Default::default(),
+//             signature_algos: Default::default(),
+//             supported_versions: Default::default(),
+//             key_share: Default::default(),
+//         }
+//     }
+// }
+
+/// Apply a JA3 fingerprint to a ClientHello [`Message`].
+///
+/// Cipher suites, curvres, and EC point formats are simply overwritten with the those specified by
+/// a JA3. For TLS extensions, the function manages to match the sort order, **optionally** add
+/// empty or hardcoded dummy records for those not in message yet but listed in JA3, and
+/// **optionally** drop existing ones from client hello payload if they are not present in JA3.
+///
+/// It returns a list of allowed unsolicited server extensions matched with the updated message.
+///
+/// # Note
+/// It is caller's responsibility to ensure that the message constitutes a valid ClientHello. Otherwise,
+/// only the protocol version in the message header is overwritten.
+pub fn overwrite_client_hello_with_fingerprint_spec(
+    msg: &mut Message,
+    fp: &FingerprintSpec,
+    add_empty_if_extension_not_in_message: bool,
+    drop_extensions_not_in_ja3: bool,
+) -> Option<Vec<ExtensionType>> {
+    use ExtensionType::*;
+    #[allow(unused_mut)]
+    let mut allowed_unsolicited_extensions = vec![ExtensionType::RenegotiationInfo];
+    trace!(
+        "overwrite client hello of {:?} with fingerprint {:?}",
+        msg,
+        fp
+    );
+    msg.version = fp
+        .ja3
+        .as_ref()
+        .map(|ja3| ja3.version_to_typed())
+        .unwrap_or(msg.version);
+    if let MessagePayload::Handshake {
+        ref mut parsed,
+        ref mut encoded,
+    } = msg.payload
+    {
+        let mut pad_per_rfc7685 = false;
+        if let HandshakeMessagePayload {
+            payload: HandshakePayload::ClientHello(ref mut chp),
+            ..
+        } = parsed
+        {
+            try_assign!(
+                chp.cipher_suites,
+                fp.ja3.as_ref().map(|ja3| ja3.ciphers_as_typed().collect())
+            );
+            // chp.client_version = ja3.version_as_typed(); // version in extension are not handled
+            match &fp.ja3 {
+                Some(ja3) => {
+                    // try to match extension order
+                    let mut new_extensions = Vec::with_capacity(if drop_extensions_not_in_ja3 {
+                        ja3.extensions.len()
+                    } else {
+                        chp.extensions.len()
+                    });
+                    let mut extmap: HashMap<u16, &ClientExtension> = chp
+                        .extensions
+                        .iter()
+                        .map(|extension| (extension.get_type().get_u16(), extension))
+                        .collect();
+                    // let greasing = fp.ja3
+                    //     .extensions
+                    //     .iter()
+                    //     .any(|&exttyp| is_grease_u16_be(exttyp));
+                    for exttyp in ja3
+                        .extensions_regreasing_as_typed()
+                        .map(|extension_type| extension_type.get_u16())
+                    {
+                        match extmap.remove(&exttyp) {
+                            Some(extension) => {
+                                // use ClientExtension::*;
+                                let mut extension = extension.clone();
+                                match &mut extension {
+                                    // // hardcode some GREASE insertions here since there is no cli option for them for now
+                                    // SupportedVersions(vers) => {
+                                    //     if !vers.iter().any(|ver| is_grease_u16_be(ver.get_u16())) {
+                                    //         vers.splice(
+                                    //             0..0,
+                                    //             [ProtocolVersion::from(grease_u16_be())],
+                                    //         );
+                                    //     }
+                                    // }
+                                    // KeyShare(ents) => {
+                                    //     if !ents
+                                    //         .iter()
+                                    //         .any(|ent| is_grease_u16_be(ent.group.get_u16()))
+                                    //     {
+                                    //         ents.splice(
+                                    //             0..0,
+                                    //             [KeyShareEntry::new(grease_u16_be().into(), &[])],
+                                    //         );
+                                    //     }
+                                    // }
+                                    _ => {}
+                                }
+                                new_extensions.push(extension)
+                            }
+                            None => {
+                                if !add_empty_if_extension_not_in_message {
+                                    continue;
+                                }
+                                trace!(
+                        "ja3 overwiting: missing extension {:?} in original chp, add an dummy one",
+                        ExtensionType::from(exttyp));
+                                // Some extension expect vectored struct, we cannot just set empty.
+                                let extpld = match ExtensionType::from(exttyp) {
+                                    // ALPN: http/1.1 + h2
+                                    ALProtocolNegotiation => {
+                                        panic!("Expect ALPN present in original message if it is listed in JA3")
+                                        // allowed_unsolicited_extensions
+                                        //     .push(ExtensionType::ALProtocolNegotiation);
+                                        // Vec::from(hex!("000c08687474702f312e31026832"))
+                                    }
+                                    // Renegotiation Info: still empty, but an additional length field
+                                    RenegotiationInfo => Vec::from(hex!("00")),
+                                    // ALPS: supported ALPN list: h2  (TODO: what is it?)
+                                    ExtensionType::Unknown(0x4469) => Vec::from(hex!("0003026832")),
+                                    // Padding => {
+                                    //     pad_per_rfc7685 = true;
+                                    //     vec![]
+                                    // }
+                                    // Compress Certificate: Rustls does not support it.
+                                    // Chrome use 02 00 02 (brotli).
+                                    // By setting it non-empty, we risk at TLS negotiation error.
+                                    ExtensionType::Unknown(0x001b) => Vec::from(hex!("020002")),
+                                    _ => vec![],
+                                };
+                                let extension = ClientExtension::Unknown(UnknownExtension {
+                                    typ: ExtensionType::from(exttyp),
+                                    payload: Payload(extpld),
+                                });
+                                // Codec works fine with UnknownExtension
+                                new_extensions.push(extension);
+                            }
+                        }
+                    }
+                    if !extmap.is_empty() {
+                        trace!("ja3 overwriting: extension {:?} in original chp not present in ja3, prepend to end: {}", extmap, !drop_extensions_not_in_ja3);
+                        if !drop_extensions_not_in_ja3 {
+                            // there might be some extensions in CHP that are not present in ja3
+                            new_extensions
+                                .extend(extmap.into_iter().map(|(_typ, ext)| ext.to_owned()));
+                        }
+                    }
+                    chp.extensions = new_extensions;
+                }
+                None => {}
+            }
+
+            // rewrite extenssion values
+            for extension in chp.extensions.iter_mut() {
+                use ClientExtension::*;
+                match extension {
+                    NamedGroups(groups) => {
+                        try_assign!(
+                            *groups,
+                            fp.ja3
+                                .as_ref()
+                                .map(|ja3| ja3.curves_regreasing_as_typed().collect())
+                        );
+                    }
+                    ECPointFormats(formats) => {
+                        try_assign!(
+                            *formats,
+                            fp.ja3
+                                .as_ref()
+                                .map(|ja3| ja3.point_formats_as_typed().collect())
+                        );
+                    }
+                    // ALProtocolN
+                    Protocols(alpn) => {
+                        try_assign!(
+                            *alpn,
+                            fp.alpn.as_ref().map(|alpn| {
+                                ProtocolNameList::from_slices(
+                                    &alpn.iter().map(|proto| &proto[..]).collect::<Vec<_>>(),
+                                )
+                            })
+                        );
+                    }
+                    SupportedVersions(vers) => {
+                        try_assign!(
+                            *vers,
+                            fp.supported_versions.as_ref().map(|vers| vers
+                                .iter()
+                                .map(|&ver| try_regrease_u16_be(ver).into())
+                                .collect())
+                        )
+                    }
+                    SignatureAlgorithms(algos) => {
+                        dbg!(&fp.signature_algos);
+                        try_assign!(
+                            *algos,
+                            dbg!(fp.signature_algos.as_ref().map(|ref algos| algos
+                                .iter()
+                                .map(|&algo| try_regrease_u16_be(algo).into())
+                                .collect()))
+                        );
+                        dbg!(algos);
+                    }
+                    KeyShare(entries) => {
+                        if let Some(fpents) = fp.key_share.as_ref() {
+                            // .map(|ent| {
+                            //     ent.iter()
+                            //         .map(|&ent| KeyShareEntry::new(ent.into(), &[]))
+                            //         .collect()
+                            //     }
+                            // )
+                            let mut oldentmap: HashMap<_, _> = entries
+                                .iter()
+                                .map(|ent| (ent.group.get_u16(), ent))
+                                .collect();
+                            //  {
+                            let mut new_entries = vec![];
+                            for &ent in fpents {
+                                match oldentmap.remove(&ent) {
+                                    Some(entry) => new_entries.push(entry.clone()),
+                                    None => {
+                                        if !is_grease_u16_be(ent) {
+                                            warn!("TLS Key Share of curve {} is not present in original ClientHello. A empty payload is used, which makes traffic look distinctive. It should not be used.", ent);
+                                        }
+                                        new_entries.push(KeyShareEntry::new(
+                                            try_regrease_u16_be(ent).into(),
+                                            &[0x00],
+                                        ));
+                                    }
+                                }
+                            }
+                            // ignore remaining entries in oldentmap, if any
+                            *entries = new_entries;
+                            //     }
+                        }
+
+                        // try_assign!(
+                        //     *ents,
+                        //     fp.key_share.as_ref().map(|ref ent| {
+                        //         ent.iter()
+                        //             .map(|&ent| KeyShareEntry::new(ent.into(), &[]))
+                        //             .collect()
+                        //     })
+                        // )
+                    }
+                    Unknown(UnknownExtension {
+                        typ: ExtensionType::Padding,
+                        ref mut payload,
+                    }) => {
+                        payload.0.clear();
+                        pad_per_rfc7685 = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if pad_per_rfc7685 {
+            // previous steps ensure padding header is included already
+            // Padding as defined in RFC7685: pad the payload to at least 512 bytes
+            // ref: https://datatracker.ietf.org/doc/html/rfc7685#section-4
+            let pldlen = parsed.get_encoding().len();
+            let padlen = RFC7685_PADDING_TARGET.saturating_sub(pldlen);
+            trace!(padlen, "ja3 overwiting: apply rfc7685 padding");
+            if padlen != 0 {
+                if let HandshakeMessagePayload {
+                    payload: HandshakePayload::ClientHello(ref mut chp),
+                    ..
+                } = parsed
+                {
+                    for extension in chp.extensions.iter_mut() {
+                        if let ClientExtension::Unknown(UnknownExtension {
+                            typ: ExtensionType::Padding,
+                            payload,
+                        }) = extension
+                        {
+                            payload.0.resize(padlen, 0);
+                            break;
+                        }
+                    }
+                }
+            }
+            debug!("ja3 overwriting: len of client hello msg >= 512 + 4, while padding is still applied ");
+            // TODO: strip padding if final length >= 512 + 4?
+        }
+        // Payload are stored twice in struct: one typed and one bytes. Both (or at least the
+        // latter) needs overwriting.
+        *encoded = Payload::new(parsed.get_encoding());
+    }
+    Some(allowed_unsolicited_extensions)
+}
---
+++ snowy-tunnel/src/totp.rs
@@ -0,0 +1,121 @@
+/// A simple TOTP implementation, intended to be used by reply filter
+use std::time::SystemTime;
+
+use crate::utils::possibly_insecure_hash_with_key;
+
+const CONTEXT: &[u8] = b"TOTP for the secure tunnel under snow";
+
+#[derive(Debug, Clone)]
+pub struct Totp {
+    key: [u8; 32],
+    step: u64,
+    skew: u8,
+}
+
+impl Totp {
+    pub fn new(key: impl AsRef<[u8]>, step_in_secs: u64, skew: u8) -> Self {
+        Totp {
+            key: possibly_insecure_hash_with_key(CONTEXT, key),
+            step: step_in_secs,
+            skew,
+        }
+    }
+
+    pub fn generate<const N: usize>(&self, time: u64) -> [u8; N] {
+        <[u8; N]>::try_from(
+            &possibly_insecure_hash_with_key(self.key, (time / self.step).to_be_bytes())[..N],
+        )
+        .unwrap()
+    }
+
+    #[inline(always)]
+    pub fn generate_current<const N: usize>(&self) -> [u8; N] {
+        self.generate(system_now())
+    }
+
+    #[inline(always)]
+    pub fn sign<const N: usize>(&self, time: u64, nonce: impl AsRef<[u8]>) -> [u8; N] {
+        <[u8; N]>::try_from(&possibly_insecure_hash_with_key(self.generate::<N>(time), nonce)[..N])
+            .unwrap()
+    }
+
+    #[inline(always)]
+    pub fn sign_current<const N: usize>(&self, nonce: impl AsRef<[u8]>) -> [u8; N] {
+        self.sign(system_now(), nonce)
+    }
+
+    pub fn check<const N: usize>(&self, token: &[u8; N], time: u64) -> bool {
+        let basestep = time / self.step - (self.skew as u64);
+        for i in 0..=self.skew * 2 {
+            let step_time = (basestep + (i as u64)) * (self.step as u64);
+            if &self.generate(step_time) == token {
+                return true;
+            }
+        }
+        false
+    }
+
+    #[inline(always)]
+    pub fn check_current<const N: usize>(&self, token: &[u8; N]) -> bool {
+        self.check(token, system_now())
+    }
+
+    pub fn verify<const N: usize>(
+        &self,
+        nonce: impl AsRef<[u8]>,
+        sig: &[u8; N],
+        time: u64,
+    ) -> bool {
+        let nonce = nonce.as_ref();
+        let basestep = time / self.step - (self.skew as u64);
+        for i in 0..=self.skew * 2 {
+            let step_time = (basestep + (i as u64)) * (self.step as u64);
+            if &self.sign(step_time, nonce) == sig {
+                return true;
+            }
+        }
+        false
+    }
+    #[inline(always)]
+    pub fn verify_current<const N: usize>(&self, nonce: impl AsRef<[u8]>, sig: &[u8; N]) -> bool {
+        self.verify(nonce, sig, system_now())
+    }
+}
+
+#[inline(always)]
+fn system_now() -> u64 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Now is after UNIX epoch")
+        .as_secs()
+}
+
+// pub fn derive_psk(key: &[u8]) -> [u8; PSKLEN] {
+//     // Blake3 defines a key derive function, but blake2 does not.
+//     let mut h = Blake2s256::new();
+//     h.update(CONTEXT);
+//     h.update(key);
+//     h.finalize().into()
+// }
+
+// // trait TOTPExt {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::Totp;
+
+    #[test]
+    fn generate_and_check() {
+        let totp = Totp::new("pamyu", 60, 2);
+        assert!(totp.check_current(&totp.generate_current::<32>()));
+        assert!(!totp.check_current(&totp.generate::<32>(0)));
+    }
+
+    #[test]
+    fn sign_and_verify() {
+        let totp = Totp::new("pamyu", 60, 2);
+        let nonce = "00010";
+        assert!(totp.verify_current(nonce, &totp.sign_current::<32>(nonce)));
+    }
+}
